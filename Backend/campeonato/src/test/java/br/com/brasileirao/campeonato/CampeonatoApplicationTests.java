fdc930b9763c0190663e5984530fd88521d62b02
vitor <vitor.nogueira94@gmail.com>
2025-06-16 21:20:04 -0300

    Initial commit

---
+++ Backend/campeonato/src/test/java/br/com/brasileirao/campeonato/CampeonatoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brasileirao.campeonato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampeonatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
