c7bccd82c918bce0e09534080fa02990a4cd527c
vitor <vitor.nogueira94@gmail.com>
2025-07-21 22:31:14 -0300

    Develop the Login Screen

--- Frontend/portal-brasileiro-frontend/src/pages/Home.tsx
+++ Frontend/portal-brasileiro-frontend/src/pages/Home.tsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
     <div className="p-4">
-      <h1 className="text-2xl font-bold text-blue-600 mb-4">Portal Brasileiro - Home</h1>
+      <h1 className="text-2xl font-bold text-blue-600 mb-4">
+        Portal Brasileiro - Home
+      </h1>
       <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
         Clique Aqui
       </button>
